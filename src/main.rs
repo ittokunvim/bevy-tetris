8b1900fc0ec715f8508d5ff0e08d448e281608bd
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-01-10 00:21:05 +0900

    feat: :wrench: Add setup code
    Issue: #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use bevy::{
+    prelude::*,
+    log::LogPlugin,
+};
+
+const GAMETITLE: &str = "テトリス";
+const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
+const BACKGROUND_COLOR: Color = Color::srgb(0.1, 0.1, 0.1);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WINDOW_SIZE.into(),
+                    title: GAMETITLE.to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+            .set(ImagePlugin::default_nearest())
+            .set(LogPlugin {
+                filter: "info,wgpu_core=warn,wgpu_hal=warn,bevy_tetris=trace".into(),
+                level: bevy::log::Level::DEBUG,
+                ..Default::default()
+            })
+        )
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+) {
+    debug!("setup camera");
+    commands.spawn(Camera2d::default());
 }
