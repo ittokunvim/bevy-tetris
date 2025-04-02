2024e3ef0f08f68c754a53c467b2127c1c6ad22e
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-03 03:08:40 +0900

    feat: :sparkles: Add block rotation process (#54)
    # Issue
    - #52
    
    ## Changes
    ブロックに回転処理を追加しました
    ブロックの形などを定義している定数を`blockdata.rs`ファイルを作成し格納しました
    
    ### Commits
    * feat: :card_file_box: Add Block data
    * feat: :technologist: Add RotationEvent
    * feat: :sparkles: Add Block rotation process

--- src/block.rs
+++ src/block.rs
@@ -5,8 +5,14 @@ use crate::{
     FIELD_SIZE,
     FIELD_POSITION,
     MoveEvent,
+    RotationEvent,
     Direction,
     FallingTimer,
+};
+use crate::blockdata::{
+    MAX_BLOCKDATA,
+    I_BLOCK,
+    I_COLOR,
 };
 
 const BLOCK_SIZE: f32 = GRID_SIZE - 1.0;
@@ -15,44 +21,42 @@ const BLOCK_POSITION: Vec3 = Vec3::new(
     FIELD_POSITION.y + GRID_SIZE / 2.0 + FIELD_SIZE.y / 2.0 - GRID_SIZE * 1.0,
     10.0,
 );
-const I_BLOCK: [usize; 8]  = [
-    0,0,0,0,
-    1,1,1,1,
-];
-// const J_BLOCK: [usize; 8]  = [
-//     1,0,0,0,
-//     1,1,1,0,
-// ];
-// const L_BLOCK: [usize; 8]  = [
-//     0,0,1,0,
-//     1,1,1,0,
-// ];
-// const O_BLOCK: [usize; 8]  = [
-//     0,1,1,0,
-//     0,1,1,0,
-// ];
-// const S_BLOCK: [usize; 8]  = [
-//     0,1,1,0,
-//     1,1,0,0,
-// ];
-// const T_BLOCK: [usize; 8]  = [
-//     0,1,0,0,
-//     1,1,1,0,
-// ];
-// const Z_BLOCK: [usize; 8]  = [
-//     0,1,1,0,
-//     1,1,0,0,
-// ];
-const I_COLOR: Color = Color::srgb(0.0, 0.0, 1.0);
-// const J_COLOR: Color = Color::srgb(0.0, 1.0, 0.0);
-// const L_COLOR: Color = Color::srgb(0.0, 1.0, 1.0);
-// const O_COLOR: Color = Color::srgb(1.0, 0.0, 0.0);
-// const S_COLOR: Color = Color::srgb(1.0, 0.0, 1.0);
-// const T_COLOR: Color = Color::srgb(1.0, 1.0, 0.0);
-// const Z_COLOR: Color = Color::srgb(1.0, 1.0, 1.0);
+
+#[derive(Resource)]
+struct CurrentBlock {
+    id: usize,
+    pos: Vec3,
+}
 
 #[derive(Component)]
-struct Block;
+struct Block(usize);
+
+impl CurrentBlock {
+    fn new() -> Self {
+        CurrentBlock {
+            id: 0,
+            pos: BLOCK_POSITION,
+        }
+    }
+
+    fn position(&self, id: usize) -> Vec3 {
+        let current_id = if self.id < I_BLOCK.len() { self.id }
+        else { panic!("self.id is too long: {}", self.id); };
+
+        for block in I_BLOCK[current_id].iter().enumerate() {
+            let (index, value) = block;
+            if id == *value {
+                let (x, y, z) = (
+                    self.pos.x + GRID_SIZE * ((index % 4) as f32),
+                    self.pos.y - GRID_SIZE * ((index / 4) as f32),
+                    self.pos.z,
+                );
+                return Vec3::new(x, y, z);
+            }
+        }
+        panic!("id not found: {}", id);
+    }
+}
 
 fn setup(
     mut commands: Commands,
@@ -60,7 +64,7 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let shape = meshes.add(Rectangle::new(BLOCK_SIZE, BLOCK_SIZE));
-    for block in I_BLOCK.iter().enumerate() {
+    for block in I_BLOCK[0].iter().enumerate() {
         let (index, value) = block;
         let (x, y, z) = (
             BLOCK_POSITION.x + GRID_SIZE * ((index % 4) as f32),
@@ -72,7 +76,7 @@ fn setup(
             Mesh2d(shape.clone()),
             MeshMaterial2d(materials.add(I_COLOR)),
             Transform::from_xyz(x, y, z),
-            Block,
+            Block(*value),
         ));
     }
 }
@@ -90,16 +94,40 @@ fn block_falling(
 fn block_movement(
     mut events: EventReader<MoveEvent>,
     mut query: Query<&mut Transform, With<Block>>,
+    mut current_block: ResMut<CurrentBlock>,
 ) {
     for event in events.read() {
         let direction = event.0;
+
+        match direction {
+            Direction::Left   => current_block.pos.x -= GRID_SIZE,
+            Direction::Right  => current_block.pos.x += GRID_SIZE,
+            Direction::Bottom => current_block.pos.y -= GRID_SIZE,
+        }
         for mut transform in &mut query {
             match direction {
                 Direction::Left   => transform.translation.x -= GRID_SIZE,
                 Direction::Right  => transform.translation.x += GRID_SIZE,
                 Direction::Bottom => transform.translation.y -= GRID_SIZE,
-                Direction::Top    => transform.translation.y += GRID_SIZE,
             }
+        }
+    }
+}
+
+fn block_rotation(
+    mut events: EventReader<RotationEvent>,
+    mut query: Query<(&Block, &mut Transform), With<Block>>,
+    mut current_block: ResMut<CurrentBlock>,
+) {
+    for event in events.read() {
+        let direction = event.0;
+        
+        if direction == Direction::Right {
+            let id = current_block.id;
+            current_block.id = if id + 1 < MAX_BLOCKDATA { id + 1 } else { 0 };
+        }
+        for (block, mut transform) in &mut query {
+            transform.translation = current_block.position(block.0);
         }
     }
 }
@@ -109,10 +137,12 @@ pub struct BlockPlugin;
 impl Plugin for BlockPlugin {
     fn build(&self, app: &mut App) {
         app
+            .insert_resource(CurrentBlock::new())
             .add_systems(Startup, setup)
             .add_systems(Update, (
                 block_falling,
                 block_movement,
+                block_rotation,
             ))
         ;
     }
--- src/key.rs
+++ src/key.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use crate::{
     BLOCK_SPEED,
     MoveEvent,
+    RotationEvent,
     Direction,
     FallingTimer,
 };
@@ -13,10 +14,10 @@ const KEY_BLOCK_RIGHT_1: KeyCode = KeyCode::ArrowRight;
 const KEY_BLOCK_RIGHT_2: KeyCode = KeyCode::KeyD;
 const KEY_BLOCK_BOTTOM_1: KeyCode = KeyCode::ArrowDown;
 const KEY_BLOCK_BOTTOM_2: KeyCode = KeyCode::KeyS;
-const KEY_BLOCK_TOP_1: KeyCode = KeyCode::ArrowUp;
-const KEY_BLOCK_TOP_2: KeyCode = KeyCode::KeyW;
+const KEY_BLOCK_ROTATION_1: KeyCode = KeyCode::ArrowUp;
+const KEY_BLOCK_ROTATION_2: KeyCode = KeyCode::KeyW;
 
-fn key_event(
+fn move_event(
     mut events: EventWriter<MoveEvent>,
     mut timer: ResMut<FallingTimer>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
@@ -32,7 +33,6 @@ fn key_event(
             &KEY_BLOCK_LEFT_1   | &KEY_BLOCK_LEFT_2   => closure(Direction::Left),
             &KEY_BLOCK_RIGHT_1  | &KEY_BLOCK_RIGHT_2  => closure(Direction::Right),
             &KEY_BLOCK_BOTTOM_1 | &KEY_BLOCK_BOTTOM_2 => closure(Direction::Bottom),
-            &KEY_BLOCK_TOP_1    | &KEY_BLOCK_TOP_2    => closure(Direction::Top),
             _ => {},
         }
     }
@@ -43,12 +43,26 @@ fn key_event(
     }
 }
 
+fn rotation_event(
+    mut events: EventWriter<RotationEvent>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    for key in keyboard_input.get_just_pressed() {
+        if key == &KEY_BLOCK_ROTATION_1 || key == &KEY_BLOCK_ROTATION_2 {
+            events.send(RotationEvent(Direction::Right));
+        }
+    }
+}
+
 pub struct KeyPlugin;
 
 impl Plugin for KeyPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(Update, key_event)
+            .add_systems(Update, (
+                move_event,
+                rotation_event,
+            ))
         ;
     }
 }
