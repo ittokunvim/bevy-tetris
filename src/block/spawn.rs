88c51aff86f08b657398e04a514e9b7778d3df55
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2025-04-10 17:45:04 +0900

    feat: :sparkles: Add gameover judjment

--- src/block/spawn.rs
+++ src/block/spawn.rs
@@ -9,6 +9,7 @@ use crate::block::{
     BLOCK_SIZE,
     RotationBlock,
     PlayerBlock,
+    Block,
 };
 use crate::blockdata::{
     I_BLOCK,
@@ -24,6 +25,7 @@ pub fn block_spawn(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut rotation_block: ResMut<RotationBlock>,
+    query: Query<&Transform, With<Block>>,
 ) {
     // イベントをチェック
     if events.is_empty() {
@@ -38,25 +40,34 @@ pub fn block_spawn(
 
     // PlayerBlockを生成
     let shape = meshes.add(Rectangle::new(BLOCK_SIZE, BLOCK_SIZE));
+    let mut init_position = BLOCK_POSITION;
 
     for (index, value) in I_BLOCK[0].iter().enumerate() {
-        // ブロックの位置を計算
-        let (x, y, z) = (
-            BLOCK_POSITION.x + GRID_SIZE * ((index % 4) as f32),
-            BLOCK_POSITION.y - GRID_SIZE * ((index / 4) as f32),
-            BLOCK_POSITION.z,
-        );
-
         // ブロックの値が0であればスキップ
         if *value == 0 {
             continue;
+        }
+
+        // ブロックの位置を計算
+        let mut position = Vec3::new(
+            init_position.x + GRID_SIZE * ((index % 4) as f32),
+            init_position.y - GRID_SIZE * ((index / 4) as f32),
+            init_position.z,
+        );
+
+        // ブロックが同士が被らないように位置を計算
+        for transform in &query {
+            if position == transform.translation {
+                position.y += GRID_SIZE;
+                init_position.y += GRID_SIZE;
+            }
         }
 
         // PlayerBlockを生成
         commands.spawn((
             Mesh2d(shape.clone()),
             MeshMaterial2d(materials.add(I_COLOR)),
-            Transform::from_xyz(x, y, z),
+            Transform::from_xyz(position.x, position.y, position.z),
             PlayerBlock(*value),
         ));
     }
