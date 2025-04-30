2422aec96d363363a0ebd01dd10acfa10702a720
ittoku <73953416+ittokunvim@users.noreply.github.com>
2025-04-30 18:54:41 +0900

    fix: :zap: fix key move event (#79)
    # Issue
    - #78
    
    ## Changes
    ブロック移動キーの長押しに対応しました。
    キーを押し続けても移動し続けることができます。
    `MoveLeftTimer, MoveRightTimer, MoveBottomTimer`リソースを追加しました。
    `key_block_moveleft, key_block_moveright, key_block_movebottom`を追加しました。
    
    
    ### Commits
    * fix: :zap: Add key_block_movebottom system (0633d6a)
    * fix: :zap: Add key_block_moveleft system (45a6108)
    * fix: :zap: Add key_block_moveright system (dedb56c)

--- src/key.rs
+++ src/key.rs
@@ -1,12 +1,15 @@
 use bevy::prelude::*;
 
 use crate::{
-    BLOCK_SPEED,
+    BLOCK_MOVE_SPEED,
     MoveEvent,
     RotationEvent,
     HardDropEvent,
     Direction,
     FallingTimer,
+    MoveLeftTimer,
+    MoveRightTimer,
+    MoveBottomTimer,
     AppState,
 };
 
@@ -20,31 +23,118 @@ const KEY_BLOCK_ROTATION_LEFT: KeyCode = KeyCode::KeyZ;
 const KEY_BLOCK_ROTATION_RIGHT: KeyCode = KeyCode::ArrowUp;
 const KEY_BLOCK_HARDDROP: KeyCode = KeyCode::Space;
 
-fn move_event(
+fn key_block_moveleft(
     mut events: EventWriter<MoveEvent>,
-    mut timer: ResMut<FallingTimer>,
+    mut moveleft_timer: ResMut<MoveLeftTimer>,
     keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
 ) {
-    info_once!("move_event");
+    info_once!("key_block_moveleft");
 
-    let mut closure = |direction: Direction| {
-        events.send(MoveEvent(direction));
-        if direction == Direction::Bottom {
-            timer.0 = FallingTimer::update_timer(BLOCK_SPEED / 2.0);
-        }
-    };
-    for key in keyboard_input.get_just_pressed() {
-        match *key {
-            KEY_BLOCK_LEFT_1   | KEY_BLOCK_LEFT_2   => closure(Direction::Left),
-            KEY_BLOCK_RIGHT_1  | KEY_BLOCK_RIGHT_2  => closure(Direction::Right),
-            KEY_BLOCK_BOTTOM_1 | KEY_BLOCK_BOTTOM_2 => closure(Direction::Bottom),
-            _ => {},
+    let block_left_keys = [KEY_BLOCK_LEFT_1, KEY_BLOCK_LEFT_2];
+
+    // ブロック左移動キー入力時
+    if keyboard_input.any_just_pressed(block_left_keys) {
+        // ブロック移動イベントを発火
+        events.send(MoveEvent(Direction::Left));
+    }
+
+    // ブロック左移動キー長押し時
+    if keyboard_input.any_pressed(block_left_keys) {
+        // ブロック左移動タイマーを進める
+        moveleft_timer.0.tick(time.delta());
+        // タイマーが切れたら
+        if moveleft_timer.0.elapsed_secs() > BLOCK_MOVE_SPEED {
+            // ブロック左移動タイマーをリセット
+            moveleft_timer.0.reset();
+            // ブロック移動イベントを発火
+            events.send(MoveEvent(Direction::Left));
         }
     }
-    for key in keyboard_input.get_just_released() {
-        if *key == KEY_BLOCK_BOTTOM_1 || *key == KEY_BLOCK_BOTTOM_2 {
-            timer.0 = FallingTimer::update_timer(BLOCK_SPEED);
+
+    // ブロック左移動キーを離した時
+    if keyboard_input.any_just_released(block_left_keys) {
+        // ブロック左移動タイマーをリセット
+        moveleft_timer.0.reset();
+    }
+}
+
+fn key_block_moveright(
+    mut events: EventWriter<MoveEvent>,
+    mut moveright_timer: ResMut<MoveRightTimer>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    info_once!("key_block_moveright");
+
+    let block_right_keys = [KEY_BLOCK_RIGHT_1, KEY_BLOCK_RIGHT_2];
+
+    // ブロック右移動キー入力時
+    if keyboard_input.any_just_pressed(block_right_keys) {
+        // ブロック移動イベントを発火
+        events.send(MoveEvent(Direction::Right));
+    }
+
+    // ブロック右移動キー長押し時
+    if keyboard_input.any_pressed(block_right_keys) {
+        // ブロック右移動タイマーを進める
+        moveright_timer.0.tick(time.delta());
+        // タイマーが切れたら
+        if moveright_timer.0.elapsed_secs() > BLOCK_MOVE_SPEED {
+            // ブロック右移動タイマーをリセット
+            moveright_timer.0.reset();
+            // ブロック移動イベントを発火
+            events.send(MoveEvent(Direction::Right));
         }
+    }
+
+    // ブロック左移動キーを離した時
+    if keyboard_input.any_just_released(block_right_keys) {
+        // ブロック右移動タイマーをリセット
+        moveright_timer.0.reset();
+    }
+}
+
+fn key_block_movebottom(
+    mut events: EventWriter<MoveEvent>,
+    mut falling_timer: ResMut<FallingTimer>,
+    mut movebottom_timer: ResMut<MoveBottomTimer>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    info_once!("key_block_movebottom");
+
+    let block_bottom_keys = [KEY_BLOCK_BOTTOM_1, KEY_BLOCK_BOTTOM_2];
+
+    // ブロック下移動キー入力時
+    if keyboard_input.any_just_pressed(block_bottom_keys) {
+        // ブロック移動イベントを発火
+        events.send(MoveEvent(Direction::Bottom));
+        // ブロック落下タイマーを一時停止
+        falling_timer.0.pause();
+        // ブロック落下タイマーをリセット
+        falling_timer.0.reset();
+    }
+
+    // ブロック下移動キー長押し時
+    if keyboard_input.any_pressed(block_bottom_keys) {
+        // ブロック下移動タイマーを進める
+        movebottom_timer.0.tick(time.delta());
+
+        if movebottom_timer.0.elapsed_secs() > BLOCK_MOVE_SPEED {
+            // ブロック下移動タイマーをリセット
+            movebottom_timer.0.reset();
+            // ブロック移動イベントを発火
+            events.send(MoveEvent(Direction::Bottom));
+        }
+    }
+
+    // ブロック下移動キー離した時
+    if keyboard_input.any_just_released(block_bottom_keys) {
+        // ブロック下移動タイマーをリセット
+        movebottom_timer.0.reset();
+        // ブロック落下タイマーの一時停止を解除
+        falling_timer.0.unpause();
     }
 }
 
@@ -57,6 +147,7 @@ fn rotation_event(
     let mut closure = |direction: Direction| {
         events.send(RotationEvent(direction));
     };
+
     for key in keyboard_input.get_just_pressed() {
         match *key {
             KEY_BLOCK_ROTATION_LEFT  => closure(Direction::Left),
@@ -85,7 +176,9 @@ impl Plugin for KeyPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(Update, (
-                move_event,
+                key_block_moveright,
+                key_block_moveleft,
+                key_block_movebottom,
                 rotation_event,
                 harddrop_event,
             ).run_if(in_state(AppState::InGame)))
